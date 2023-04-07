3fb536463489bf64d8da3eb5120c739b2756a7ae
Alejandro Jimenez Rios <alejand@oslomet.no>
2023-04-07 11:05:35 +0200

    Added Publications page and news about Publications page

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
+// Code for visits counter
 var counterContainer = document.querySelector(".website-counter");
 var resetButton = document.querySelector("#reset");
 var visitCount = localStorage.getItem("page_view");
-
 // Check if page_view entry is present
 if (visitCount) {
     visitCount = Number(visitCount) + 1;
@@ -11,7 +11,6 @@ if (visitCount) {
     localStorage.setItem("page_view", 1);
 }
 counterContainer.innerHTML = visitCount;
-
 // Adding onClick event listener
 resetButton.addEventListener("click", () => {
     visitCount = 1;
